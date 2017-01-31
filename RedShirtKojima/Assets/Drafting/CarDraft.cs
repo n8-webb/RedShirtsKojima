4ad19e84cf9e09bed5159c23da4209367a408c91
Ryanbnm666 <ryanbnm667@gmail.com>
2017-01-31 19:03:04 +0000

    Drafting push with actual drafting files

---
+++ RedShirtKojima/Assets/Drafting/CarDraft.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CarDraft : MonoBehaviour {
+
+    public GameObject triggerPrefrab;
+    List<GameObject> carDraftTriggers;
+    float timer;
+    float spawnRate;
+
+	// Use this for initialization
+	void Start () {
+        timer = 0.0f;
+        spawnRate = 0.2f;
+
+        carDraftTriggers = new List<GameObject>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        timer += Time.deltaTime;
+
+        if(timer >= spawnRate)
+        {
+            timer = 0.0f;
+            GameObject newTrigger = Instantiate(triggerPrefrab);
+            newTrigger.transform.parent = transform.parent;
+            newTrigger.transform.position = transform.position;
+            carDraftTriggers.Add(newTrigger);
+        }
+
+
+        foreach (GameObject trigger in carDraftTriggers)
+        {
+            DraftTrigger triggerScript = trigger.GetComponent<DraftTrigger>();
+            
+            if (triggerScript.getDead())
+            {
+                
+                carDraftTriggers.Remove(trigger);
+                GameObject.Destroy(trigger);
+                
+            }
+        }
+    }
+}
---
+++ RedShirtKojima/Assets/Drafting/DraftTrigger.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DraftTrigger : MonoBehaviour {
+
+    float startScale;   //Start scale of draft trigger
+    float triggerTime;  //Time it takes to scale to 0
+    float scaleTime;    //Scale to transform object by
+
+    bool dead;
+
+	// Use this for initialization
+	void Start () {
+        //Set scale to 0 time
+        triggerTime = 3.0f;
+
+        //Maths to calculate scale time
+        startScale = transform.localScale.x;
+        scaleTime = triggerTime / startScale;
+
+        dead = false;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //If scale is <= 0 the object has shrunk to nothing, destroy.
+        if(transform.localScale.x <= 0.0f)
+        {
+            dead = true;
+        }
+        else
+        {
+            //Calculate how much to scale this update
+            float scaleBy = Time.deltaTime / scaleTime;
+
+            //Scale
+            transform.localScale -= new Vector3(scaleBy, scaleBy, scaleBy);
+        }
+    }
+
+    public bool getDead()
+    {
+        return dead;
+    }
+}
